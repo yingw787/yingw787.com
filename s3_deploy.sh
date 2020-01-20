c8497aae1063ab434e2e7b84f0bfd8aa92d7a4a0
Ying Wang <yingw787.personal@gmail.com>
2020-01-20 18:19:32 -0500

    Update s3_deploy.sh remove .git/ directory from uploading.

--- s3_deploy.sh
+++ s3_deploy.sh
@@ -1,2 +1,10 @@
-aws s3 rm s3://yingw787.com --recursive --region us-east-2 --profile s3_personal
-aws s3 cp . s3://yingw787.com --recursive --region us-east-2 --profile s3_personal
+aws s3 rm s3://yingw787.com \
+    --recursive \
+    --region us-east-2 \
+    --profile s3_personal
+
+aws s3 cp . s3://yingw787.com \
+    --recursive \
+    --region us-east-2 \
+    --exclude "*.git/*" \
+    --profile s3_personal
